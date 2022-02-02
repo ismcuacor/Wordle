d7b45db5fba8722912a47e7d27192f13c783827f
Ismael <ismcuacor@gmail.com>
2022-02-02 14:29:51 +0100

    optimizado - 1

--- Wordle.py
+++ Wordle.py
@@ -8,7 +8,6 @@ for linea in fichero:
 
 abecedario={'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'ñ':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
 def eligePalabraDiccionario(nuevoDiccionario):
-    #diccionarioValores = dict.fromkeys(nuevoDiccionario)
     #le asigno a las letras un valor según el número de veces que aparezcan en el diccionario. Una vez por palabra
     for palabra in nuevoDiccionario:
         #le quito a la palabra las letras repetidas
